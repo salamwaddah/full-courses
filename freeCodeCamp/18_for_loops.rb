137ed910e88b21232e09962351802b9c87a050b8
Salam <salamwaddah@gmail.com>
2023-04-02 21:48:31 +0400

    for loops

---
+++ freeCodeCamp/18_for_loops.rb
@@ -0,0 +1,13 @@
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+for friend in friends
+  puts friend.upcase
+end
+
+friends.each do |fr|
+  puts fr
+end
+
+6.times do |index|
+  puts index
+end
