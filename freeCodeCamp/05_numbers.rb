122fdfe5d14c95b34195a2c37507c1a2285eb7f8
Salam <salamwaddah@gmail.com>
2023-04-02 16:47:36 +0400

    numbers

---
+++ freeCodeCamp/05_numbers.rb
@@ -0,0 +1,21 @@
+puts 28
+puts 38.21
+puts -14.5
+puts 5 + 9
+puts 2 ** 3
+
+num = 10.24
+puts num % 3
+
+puts("My favorite number " + num.to_s)
+
+puts num.abs
+puts num.round
+puts num.ceil
+puts num.floor
+
+puts Math.sqrt(num)
+puts Math.log(num)
+
+puts 10 / 7
+puts 10 / 7.0
