a87d5d642dfb66759b1d883bbcb9157dc8c74995
Salam <salamwaddah@gmail.com>
2023-04-02 18:14:47 +0400

    hashes

---
+++ freeCodeCamp/10_hashes.rb
@@ -0,0 +1,11 @@
+emirates = {
+  "Abu Dhabi" => "AUH",
+  :Dubai => "DXB",
+  "Sharjah" => "SHJ",
+  4 => "AJM",
+}
+
+puts emirates
+puts emirates["Sharjah"]
+puts emirates[:Dubai]
+puts emirates[4]
