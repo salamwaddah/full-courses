21ef133f74860a9c1c9bbba69a9d12072567282d
Salam <salamwaddah@gmail.com>
2023-04-02 16:27:32 +0400

    variables

---
+++ freeCodeCamp/variables.rb
@@ -0,0 +1,10 @@
+character_name = "Abdulsalam"
+character_age = "28"
+
+puts("There once was a man named " + character_name)
+puts("he was " + character_age + " years old.")
+
+character_name = "Salam"
+
+puts("He really liked the name " + character_name)
+puts("but didn't like being " + character_age + ".")
