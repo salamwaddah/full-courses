c5ac148d1d255c6538a7b8aa3616ce7b1ebd4cce
Salam <salamwaddah@gmail.com>
2023-04-02 16:40:34 +0400

    strings

---
+++ freeCodeCamp/04_strings.rb
@@ -0,0 +1,14 @@
+phrase = "   Salam   "
+
+puts phrase + " \" Waddah"
+puts phrase + " \nWaddah"
+
+puts phrase.upcase
+puts phrase.downcase
+puts phrase.strip
+puts phrase.strip.length
+puts phrase.include? "Waddah"
+puts phrase.strip[0]
+puts phrase.strip[1]
+puts phrase.strip[1, 3]
+puts phrase.index("alam")
