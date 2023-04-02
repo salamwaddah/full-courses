695b95c254df92619bb161d399528f0c0c208d07
Salam <salamwaddah@gmail.com>
2023-04-02 21:07:42 +0400

    ifs continued

--- freeCodeCamp/13_ifs.rb
+++ freeCodeCamp/13_ifs.rb
@@ -10,3 +10,17 @@ elsif !is_male and is_tall
 else
   puts "You are not male and not tall"
 end
+
+#####
+
+def max(num1, num2, num3)
+  if num1 >= num2 and num1 >= num3
+    return num1
+  elsif num2 >= num1 and num2 >= num3
+    return num2
+  else
+    return num3
+  end
+end
+
+puts max(1, 20, 3)
