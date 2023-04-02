599f2d0a44da53f5786249b41562a54a4882593e
Salam <salamwaddah@gmail.com>
2023-04-02 18:09:41 +0400

    arrays

---
+++ freeCodeCamp/09_arrays.rb
@@ -0,0 +1,16 @@
+friends = Array["Kevin", "Karen", "Oscar", "Andy"]
+
+puts friends
+puts friends[0]
+puts friends[-1]
+puts friends[0, 2]
+puts friends.length
+puts friends.include? "Salam"
+puts friends.reverse
+puts friends.sort
+
+new_friends = Array.new
+new_friends[0] = "Salam"
+new_friends[5] = "Waddah"
+
+puts new_friends
