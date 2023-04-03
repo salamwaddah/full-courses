9972280af596463b3b27bccaa703fb01bf380bd4
Salam <salamwaddah@gmail.com>
2023-04-03 10:48:45 +0400

    classes

---
+++ freeCodeCamp/23_classes.rb
@@ -0,0 +1,15 @@
+class Book
+  attr_accessor :title, :author, :pages
+
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
+end
+
+book1 = Book.new("Harry Potter", "JK Rowling", 400)
+
+puts book1.title
+puts book1.author
+puts book1.pages
