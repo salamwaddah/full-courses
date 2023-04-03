345f1e8539cc257ede622d408e884ba0c23a7e80
Salam <salamwaddah@gmail.com>
2023-04-03 11:14:09 +0400

    modules

---
+++ freeCodeCamp/27_modules.rb
@@ -0,0 +1,4 @@
+require_relative 'modules/useful_tools'
+include Tools
+
+Tools.say_hi("Salam")
---
+++ freeCodeCamp/modules/useful_tools.rb
@@ -0,0 +1,9 @@
+module Tools
+  def say_hi(name)
+    puts "hello #{name}"
+  end
+
+  def say_bye(name)
+    puts "bye #{name}"
+  end
+end
