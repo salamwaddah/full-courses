0c33fe7936717473e2a8c713f3730e7a3fed804d
Salam <salamwaddah@gmail.com>
2023-04-18 10:35:00 +0400

    ror: resources route

--- ruby-on-rails/blog/config/routes.rb
+++ ruby-on-rails/blog/config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 end
