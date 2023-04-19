c3f9be2c3bd958418e83cb14503a44ae3c1065ac
Salam <salamwaddah@gmail.com>
2023-04-19 15:55:04 +0400

    ror: article, comment statuses

--- ruby-on-rails/blog/app/models/article.rb
+++ ruby-on-rails/blog/app/models/article.rb
@@ -3,4 +3,11 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  VALID_STATUSES = %w[public private archived]
+
+  def archived?
+    status == 'archived'
+  end
 end
--- ruby-on-rails/blog/app/models/comment.rb
+++ ruby-on-rails/blog/app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  VALID_STATUSES = %w[public private archived]
+
+  def archived?
+    status == 'archived'
+  end
 end
