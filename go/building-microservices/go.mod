d8a29d9cf9eac7ef2d2068d227f183f6b8440629
Salam <salamwaddah@gmail.com>
2023-05-15 17:52:21 +0400

    go: hello, goodbye handlers

---
+++ go/building-microservices/go.mod
@@ -0,0 +1,3 @@
+module github.com/salamwaddah/full-courses/go/building-microservices
+
+go 1.20
---
+++ go/building-microservices/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/salamwaddah/full-courses/go/building-microservices/handlers"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	http.ListenAndServe(":9090", sm)
+}
