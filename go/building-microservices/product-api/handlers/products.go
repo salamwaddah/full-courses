b0cfd61be0be4be617500a2f8035140f50a029e1
Salam <salamwaddah@gmail.com>
2023-05-23 14:00:29 +0400

    go: product handler

---
+++ go/building-microservices/product-api/handlers/products.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+type Product struct {
+	l *log.Logger
+}
+
+func NewProduct(l *log.Logger) *Product {
+	return &Product{l}
+}
+
+func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+
+}
