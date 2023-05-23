01bc9e16f5351d20915a091cb23b00fda4a0f80b
Salam <salamwaddah@gmail.com>
2023-05-23 13:47:24 +0400

    go: configure http server with graceful shutdown

--- go/building-microservices/main.go
+++ go/building-microservices/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"github.com/salamwaddah/full-courses/go/building-microservices/handlers"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
+
+	"github.com/salamwaddah/full-courses/go/building-microservices/handlers"
 )
 
 func main() {
@@ -13,9 +17,37 @@ func main() {
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodBye(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", hh)
+	serveMux.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", sm)
+	// create a new http server
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      serveMux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// wrap ListenAndServe in a goroutine, so it doesn't block the graceful shutdown
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// create a signal channel to listen for os interruptions
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	// block until a signal is received
+	sig := <-sigChan
+
+	// setup graceful shutdown
+	l.Println("Received terminate, graceful shutdown", sig)
+	timeoutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(timeoutContext)
 }
